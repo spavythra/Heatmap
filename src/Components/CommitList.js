0cfd7c078ba39ecfef780b5fecbe9b38caef5088
Pavithra Subramaniyam <pavithra.subramaniym@tuni.fi>
2022-07-10 19:33:37 +0300

    (CommitList.js): commits data transfered from RepoList.js to CommitList.js

--- src/Components/CommitList.js
+++ src/Components/CommitList.js
@@ -3,27 +3,9 @@ import axios from 'axios';
 
 function CommitList(props) {
     const [commitList, setCommitList] = useState([])
-    console.log(props.public_list.public_repo_data)
-    console.log(Object.values(props.public_list.public_repo_data))
+    console.log(props.public_list.commitList)
 
-    useEffect(() =>{
-    for( let [key,value] of Object.entries(props.public_list.public_repo_data)){
-        if(value==="public"){
-            console.log("commitList")
-                axios.get(`https://api.github.com/repos/${process.env.REACT_APP_USER}/${key}/commits`)
-                .then((response) => {
-                    setCommitList( response.data);
-                
-            })
-            
-            
-        }
-    }}, []);
-
-    console.log(commitList)
-    commitList.map((item) =>{
-        console.log("j")
-    })
+    
   return (
     <div>list</div>
   )
