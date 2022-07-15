0cb93f0ea3f4c6ff69eaf6accce4c257acd7c1a1
Pavithra Subramaniyam <pavithra.subramaniym@tuni.fi>
2022-07-16 00:02:26 +0300

    (store): removed store and code cleanup

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import search from './img/search.png'
 function App() {
 const [user, setUser] = useState("")
 // const [loading, setLoading] = useState(false)
-const [repos, setRepos] = useState([])
-const [privateRepos, setPrivateRepos] = useState([])
+const [setRepos] = useState([])
+const [setPrivateRepos] = useState([])
 const [commits, setCommits] = useState([])
 const [count, setCount] = useState([])
 const [repoCommit, setRepoCommit] = useState([])
@@ -18,60 +18,61 @@ let total_details = [];
 let public_repo_name =[]
 let private_repo_name =[]
 
-function handleSubmit(e) {
+function handleSubmit(e) 
+{
   e.preventDefault();
   searchRepos();
 }
 
-function commit_message(date, msg){
+function commit_message(date, msg)
+{
   this.date = date;
   this.message = msg;
-  // this.repo_name = repo_name;
 }
 
-function repo_info(date, msg,repo_name){
+function repo_info(date, msg,repo_name)
+{
   this.date = date;
   this.message = msg;
   this.repo_name = repo_name;
 }
 
-// function to count the number of
-    //occurance of the same date
-    function findOcc(arr, key)
-    {
-      // console.log(arr)
-      let arr2 = [];
-        
-      arr.forEach((x)=>{
-           
-        // Checking if there is any object in arr2
-        // which contains the key value
-         if(arr2.some((val)=>{ return val[key] === x[key] }))
-         {
-           // If yes! then increase the occurrence by 1
-           arr2.forEach((k)=>{
-             if(k[key] === x[key]){ 
-               k["count"]++
-             }
-          })  
-         }
-         else
-         {
-           // If not! Then create a new object initialize 
-           // it with the present iteration key's value and 
-           // set the occurrence to 1
-           let a = {}
-           a[key] = x[key]
-           a["count"] = 1
-           arr2.push(a);
-         }
-      }) 
-      return arr2
-    }
+// count the number of occurances of the same date
+function date_occurences(arr, key)
+{
+  let tempArray = [];
     
-      // -----end of function---------
+  arr.forEach((x)=>{
+        
+    // Checking if there is any object in tempArray
+    // which contains the key value
+      if(tempArray.some((val)=>{ return val[key] === x[key] }))
+      {
+        // If yes! then increase the occurrence by 1
+        tempArray.forEach((k)=>{
+          if(k[key] === x[key]){ 
+            k["count"]++
+          }
+      })  
+      }
+      else
+      {
+        // If not! Then create a new object initialize 
+        // it with the present iteration key's value and 
+        // set the occurrence to 1
+        let a = {}
+        a[key] = x[key]
+        a["count"] = 1
+        tempArray.push(a);
+      }
+  }) 
+  return tempArray
+}
+// -----end of function---------
 
-function searchRepos() {
+
+function searchRepos() 
+{
   axios.get(`https://api.github.com/search/repositories?q=user:${user}+is:public`).then((res) =>{
               res.data.items.map((item) =>{
                 public_repo_name.push(item.name)
@@ -81,11 +82,14 @@ function searchRepos() {
   })
 }
 
-function searchPrivateRepos(public_data) {
+function searchPrivateRepos(public_data) 
+{
   if(!process.env.REACT_APP_TOKEN){
     setPrivateRepos(public_data)
     renderRepo(public_data)
-  } else {
+  } 
+  else 
+  {
     axios.get(`https://api.github.com/search/repositories?q=user:${user}+is:private`,{
                 headers: {
                     "Authorization" : `Bearer ${process.env.REACT_APP_TOKEN}`
@@ -98,39 +102,39 @@ function searchPrivateRepos(public_data) {
     setPrivateRepos(private_repo_name)
     let total_commits = public_data.concat(private_repo_name)
     renderRepo(total_commits)
-    // console.log(total_commits)
   })
   }
   
 }
 
-function renderRepo(repo){
-  // console.log(repo)
-
-  if(!process.env.REACT_APP_TOKEN){
-    for(let i= 0; i< repo.length;i++){
-
+function renderRepo(repo)
+{
+  if(!process.env.REACT_APP_TOKEN)
+  {
+    for(let i= 0; i< repo.length;i++)
+    {
       axios.get(`https://api.github.com/repos/${user}/${repo[i]}/commits`
               ).then(res =>{
         res.data.map((item) =>{
           const exact_date = (item.commit.author.date.split("T",1)).toString();
-                      var commit_messages = new commit_message(exact_date, item.commit.message)
-                      var repo_msg = new repo_info(exact_date, item.commit.message,repo[i])
-                      total_list.push(commit_messages)
-                      total_details.push(repo_msg)
+          var commit_messages = new commit_message(exact_date, item.commit.message)
+          var repo_msg = new repo_info(exact_date, item.commit.message,repo[i])
+          total_list.push(commit_messages)
+          total_details.push(repo_msg)
       })
-      if(i===(repo.length-1)){
+      if(i===(repo.length-1))
+      {
         setCommits(total_list)
         setRepoCommit(total_details)
         countCommit(total_list)
-        
       }
       })
     }
-    // console.log(total_list)
-  }else {
-    for(let i= 0; i< repo.length;i++){
-
+  }
+  else 
+  {
+    for(let i= 0; i< repo.length;i++)
+    {
       axios.get(`https://api.github.com/repos/${user}/${repo[i]}/commits`,{
                   headers: {
                       "Authorization" : `Token ${process.env.REACT_APP_TOKEN}`
@@ -143,7 +147,8 @@ function renderRepo(repo){
                 total_list.push(commit_messages)
                 total_details.push(repo_msg)
       })
-      if(i===(repo.length-1)){
+      if(i===(repo.length-1))
+      {
         setCommits(total_list)
         setRepoCommit(total_details)
         countCommit(total_list)
@@ -151,17 +156,15 @@ function renderRepo(repo){
       }
       })
     }
-    // console.log(total_list)
   }
 
   
   
 }
 
-function countCommit(commit){
-  // console.log(commit)
-  let commit_values = findOcc(commit, "date")
-  // console.log(commit_values)
+function countCommit(commit)
+{
+  let commit_values = date_occurences(commit, "date")
   setCount(commit_values)
   setShowDetail(true)
  
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -6,11 +6,11 @@ import '../App.css';
 import Message from './Message';
 
 
-function Form({commits,count,repoCommit}) {
-
+function Form({commits,count,repoCommit}) 
+{
     const [day, setDay] = useState("")
     const [showDetails, setShowDetail] = useState(false)
-    // console.log(count)
+
 //getting today's date using Date 
 //coverting date to yyyy/mm/dd format
 const today = new Date().toISOString().split("T")[0];
@@ -41,70 +41,71 @@ const randomValues = getRange(365).map(index => {
   // -----end of function---------
   
   
-  //function to check the date from the available commit list
-        //and return its count value
-        function getRandomInt(date) 
+//function to check the date from the available commit list
+//and return its count value
+function getRandomInt(date) 
+{
+    for(var i=0; i<commits.length; i++)
+    {
+    for (var j=0; j<count.length; j++)
+    {
+        if(count[j].date === date.toISOString().slice(0, 10))
         {
-          for(var i=0; i<commits.length; i++)
-          {
-            for (var j=0; j<count.length; j++)
-            {
-              if(count[j].date === date.toISOString().slice(0, 10))
-              {
-                return count[j].count ;
-              } 
-              else
-              {
-                continue
-              }
-            }
-            return 0;
-          }
-            
+        return count[j].count ;
+        } 
+        else
+        {
+        continue
         }
+    }
+    return 0;
+    }  
+}
 
-        function handleSubmit(e) {
-            setDay(e.date.toISOString().split("T")[0])
-            setShowDetail(true)
-          }
+function handleSubmit(e) 
+{
+    setDay(e.date.toISOString().split("T")[0])
+    setShowDetail(true)
+}
   
 
-  return (
+return (
     <div>
-    <div className='calender'><CalendarHeatmap
-    startDate={shiftDate(today, -365)}
-    endDate={today}
-    values={randomValues}
-    classForValue={value => {
-      
-      if (value.count===0) {
-        return 'color-empty';
-      }else if(value.count<3) {return `color-github-1`}
-      else if(value.count<6) {return `color-github-2`}
-      else if(value.count<9) {return `color-github-3`}
-      else if(value.count>=9)  {return `color-github-4`}
-      
-    }}
-    tooltipDataAttrs={value => {
-      // console.log(value.date)
-      return {
-        'data-tip': `${value.date.toISOString().slice(0, 10)} has ${
-          value.count
-        } commits`,
-      };
-    }}
-    showWeekdayLabels={true}
-    onClick={handleSubmit}
-  />
-  <div className='box'><p>Less </p>
-  <div className='color-box1'></div>
-  <div className='color-box2'></div>
-  <div className='color-box3'></div>
-  <div className='color-box4'></div>
-  <div className='color-box5'></div><p> More</p></div>
-  <ReactTooltip />
-  </div>
-  { showDetails && <Message day={day} repoCommit={repoCommit}/>}
+        <div className='calender'><CalendarHeatmap
+            startDate={shiftDate(today, -365)}
+            endDate={today}
+            values={randomValues}
+            classForValue={value => {
+            
+            if (value.count===0) 
+            {
+                return 'color-empty';
+            }
+            else if(value.count<3) {return `color-github-1`}
+            else if(value.count<6) {return `color-github-2`}
+            else if(value.count<9) {return `color-github-3`}
+            else if(value.count>=9){return `color-github-4`}
+            
+            }}
+            tooltipDataAttrs={value => {
+            return {
+                'data-tip': `${value.date.toISOString().slice(0, 10)} has ${
+                value.count
+                } commits`,
+            };
+            }}
+            showWeekdayLabels={true}
+            onClick={handleSubmit}
+        />
+        <div className='box'><p>Less </p>
+        <div className='color-box1'></div>
+        <div className='color-box2'></div>
+        <div className='color-box3'></div>
+        <div className='color-box4'></div>
+        <div className='color-box5'></div><p> More</p></div>
+        <ReactTooltip />
+    </div>
+    { showDetails && <Message day={day} repoCommit={repoCommit}/>}
   </div>
   )
 }
